7d0accb604bca5103ad0264e7e39a7b7a15bfb35
DaniStonks <200221067@estudantes.ips.pt>
2022-02-20 23:24:15 +0000

    passport ta estranho

--- IberGames/app.js
+++ IberGames/app.js
@@ -1,20 +1,37 @@
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+var passport = require("passport");
+var session = require("express-session");
 var express = require("express");
 var bodyParser = require("body-parser");
+
+var mysql = require('mysql2/promise');
+var MySQLStore = require('express-mysql-session')(session);
+var options = require("./config/options.json");
 
 var routes = require("./routes/index");
 var authRoutes = require("./routes/auth");
 
 var app = express();
+var connection = mysql.createPool(options.mysql);
+var sessionStore = new MySQLStore({}, connection);
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
+app.use(passport.authenticate('session'));
+
 
 app.use("/", routes);
 app.use("/", authRoutes);
--- IberGames/routes/auth.js
+++ IberGames/routes/auth.js
@@ -9,19 +9,31 @@ const options = require("../config/options.json");
 
 const router = express.Router();
 
-passport.use(new LocalStrategy(function verify(username, password, done) {
+passport.use(new LocalStrategy(function verify(username, password, cb) {
     let connection = mysql.createConnection(options.mysql);
     connection.connect();
     connection.query('SELECT Regist_pass FROM Registado WHERE Regist_name = ?', [username], function (err, row) {
-        if (err) { return done(err); }
-        if (!row) { return done(null, false, { message: 'Incorrect username or password.' }); }
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
 
-        if(!(row.Regist_pass == password)){
-            return done(null, false, { message: 'Incorrect username or password.' });
+        if (row[0].Regist_pass !== password) {
+            return cb(null, false, { message: 'Incorrect username or password.' });
         }
-        return done(null, row);
+        return cb(null, row);
     });
 }));
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
 
 /*
 A usar quando se der reset na bd e usar-se hashing
@@ -49,6 +61,6 @@ router.get("/login", function (req, res, next) {
 router.post('/login/password', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login'
-  }));
+}));
 
 module.exports = router;
