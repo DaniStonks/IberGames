e766d71ad07de9d4afb29fe24465358825041c54
Rafael-NSilva <80034802+Rafael-NSilva@users.noreply.github.com>
2022-02-24 02:11:52 +0000

    Forum SwitchTheme a funcionar
    nao consigo fazer a cena do Dom esta demasiado confuso.

--- IberGames/public/scripts/app.js
+++ IberGames/public/scripts/app.js
@@ -11,3 +11,22 @@ function changeMainPageTheme(){
     currentSheet.classList.add("light");
   }
 }
+
+function changeForumPageTheme(){
+  let currentSheet = document.getElementById("activeTheme");
+  if(currentSheet.classList[0] == 'light'){
+    currentSheet.setAttribute("href", "/stylesheets/darkModeForum.css");
+    currentSheet.classList.remove("light");
+    currentSheet.classList.add("dark");
+  }
+  else{
+    currentSheet.setAttribute("href","/stylesheets/whiteModeForum.css");
+    currentSheet.classList.remove("dark");
+    currentSheet.classList.add("light");
+  }
+}
+
+
+/**** ****/
+const newsImage = document.getElementsByClassName("game-image");
+const newsTitle = document.getElementsByClassName("news_title");
